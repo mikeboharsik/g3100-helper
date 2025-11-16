a898a56a11ffeebe8fc6c68f887f34de2c65f81e
Mike Boharsik <mboharsik@gmail.com>
2025-11-16 11:19:23 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Why
+I hate using the UI to access my router information and there are situations where programmatic access is desirable.
+
+# Why puppeteer
+I initially wanted to reverse-engineer how the router's client implements its auth flow but
+1. they seem to have made arbitrarily dumb decisions that coincidentally make it more difficult to reverse-engineer (e.g. hashing the password on the client before sending it to the backend)
+2. even if I recreated the client behavior in my own library, there is a decent chance that a future router update would render all the reverse-engineering work pointless
+
+I understand it's kinda crazy to use a headless browser just to access some router data, but it strikes me as being the least brittle long-term solution. And if the implementation breaks, it should be a simple matter of updating some selectors.
+
+# Usage
+Set the environment variable `G3100_PASSWORD` to the password of the `admin` user of the router. Then relevant functions can be imported as follows:
+```
+import { getLocalIpAddresses, getPublicIpAddress } from 'g3100-helper';
+
+const localIpAddresses = await getLocalIpAddresses();
+const publicIpAddress = await getPublicIpAddress();
+console.log({ localIpAddresses, publicIpAddress });
+```
---
+++ index.mjs
@@ -0,0 +1,141 @@
+import puppeteer from 'puppeteer';
+
+const baseUri = 'https://myfiosgateway.com';
+const loginUri = `${baseUri}/login.cgi`;
+const logoutUri = `${baseUri}/logout.cgi`;
+const dnsServerUri = `${baseUri}/cgi/cgi_dns_server.js`;
+const statusUri = `${baseUri}/cgi/cgi_status.js`;
+
+async function login(page) {
+	await page.goto(baseUri);
+	await page.locator('.vz-input').fill(process.env.G3100_PASSWORD);
+	await page.locator('.btn-primary').click();
+
+	const loginResponse = await page.waitForResponse(
+		response =>
+			response.url() === loginUri && response.request().method() === 'POST',
+	);
+
+	if (loginResponse.status() !== 302) {
+		throw new Error('Failed to login');
+	}
+}
+
+async function logout(page) {
+	await page.reload();
+	await page.locator('#userIconArrow').click();
+	await page.locator("div[aria-label='Sign Out']").click();
+
+	const logoutResponse = await page.waitForResponse(
+		response =>
+			response.url() === logoutUri && response.request().method() === 'POST',
+	);
+
+	if (logoutResponse.status() !== 302) {
+		throw new Error('Failed to logout');
+	}
+}
+
+function getEnvAppropriatePuppeteerSettings() {
+	if (process.env.SHELL?.includes('bash')) {
+		return {
+			// this was necessary to get it running on RPi
+			// sudo apt install chromium-browser
+			executablePath: '/usr/bin/chromium-browser',
+			args: [
+				'--ignore-certificate-errors',
+				'--no-sandbox'
+			]
+		};
+	}
+
+	return {};
+}
+
+function getOriginalSettings() {
+	const emitWarning = process.emitWarning;
+	const tlsSetting = process.env.NODE_TLS_REJECT_UNAUTHORIZED;
+
+	process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+	process.emitWarning = () => {};
+
+	return { emitWarning, tlsSetting };
+}
+
+function restoreOriginalSettings(settings) {
+	process.env.NODE_TLS_REJECT_UNAUTHORIZED = settings.tlsSetting;
+	process.emitWarning = settings.emitWarning;
+}
+
+function verifyExpectedEnvVars() {
+	if (!process.env.G3100_PASSWORD) {
+		throw new Error(`Environment variable G3100_PASSWORD is required`);
+	}
+}
+
+function getLocalIpAddressesFromTextResponse(text) {
+	try {
+		const trimmed = text.trim();
+		let match = trimmed.match(/^.*get_dhcp_hosts.*\n/)[0].match(/\[.*\]/)[0];
+		let sorted = eval(match).toSorted(([, a], [, b]) => a > b ? 1 : a < b ? -1 : 0);
+		let result = sorted.reduce((acc, cur) => { acc[cur[0]] = cur[1]; return acc; }, {});
+		return result;
+	} catch (e) {
+		console.log('Failed to match against public IP address', text);
+	}
+}
+
+async function getLocalIpAddressesContent(sysauthCookie) {
+	return await fetch(dnsServerUri, { headers: { Cookie: `sysauth=${sysauthCookie}` }})
+		.then(r => r.text())
+		.then(r => getLocalIpAddressesFromTextResponse(r));
+}
+
+function getPublicIpAddressFromTextResponse(text) {
+	try {
+		const trimmed = text.trim();
+		return trimmed.match(/get_wan4_ip.*\"(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\"/)[1];
+	} catch (e) {
+		console.log('Failed to match against public IP address', text);
+	}
+}
+
+async function getPublicIpAddressContent(sysauthCookie) {
+	return await fetch(statusUri, { headers: { Cookie: `sysauth=${sysauthCookie}` }})
+		.then(r => r.text())
+		.then(r => getPublicIpAddressFromTextResponse(r));
+}
+
+function getWrappedFunction(contentFunction) {
+	return async function genericFunction() {
+		verifyExpectedEnvVars();
+
+		const originalSettings = getOriginalSettings();
+
+		const launchSettings = getEnvAppropriatePuppeteerSettings();
+		const browser = await puppeteer.launch(launchSettings);
+
+		const page = await browser.newPage();
+		await page.setViewport({ width: 1280, height: 720 });
+		page.setDefaultTimeout(6000);
+
+		let result;
+		try {
+			await login(page);
+
+			const sysauthCookie = (await browser.cookies()).find(e => e.name === 'sysauth').value;
+			result = await contentFunction(sysauthCookie);
+		} catch (e) {
+			console.error('Failed to load content', e);
+		} finally {
+			await logout(page);
+			await browser.close();
+
+			restoreOriginalSettings(originalSettings);
+		}
+		return result;
+	}
+}
+
+export const getPublicIpAddress = getWrappedFunction(getPublicIpAddressContent);
+export const getLocalIpAddresses = getWrappedFunction(getLocalIpAddressesContent);
