1b05638fa8a0051b765f3016b84f06a8fce30655
Mike Boharsik <mboharsik@gmail.com>
2025-11-24 10:26:38 -0500

    test: automate the loading of fixture files

--- test/configureFixtures.mjs
+++ test/configureFixtures.mjs
@@ -2,14 +2,23 @@ import fs from 'fs';
 
 import { jest } from '@jest/globals';
 
+const fixturesPath = './test/fixtures';
+const functionFixtureMap = {
+	'getLocalIpAddressesContent': '3.6.0.6_cgi_dns_server.js',
+	'getRouterStatusContent': '3.6.0.6_cgi_status.js',
+};
+
 export default function configureFixtures() {
-	const dnsServer = fs.readFileSync('./test/fixtures/3.6.0.6_cgi_dns_server.js', 'utf8');
-	const status = fs.readFileSync('./test/fixtures/3.6.0.6_cgi_status.js', 'utf8');
+	const mockedExports = Object.entries(functionFixtureMap)
+		.reduce((acc, [mockedFunctionName, fixtureFileName]) => {
+			acc[mockedFunctionName] = async (sysauthCookie) => {
+				expect(sysauthCookie).toBeDefined();
+				return fs.readFileSync(`${fixturesPath}/${fixtureFileName}`, 'utf8');
+			}
+			return acc;
+		}, {});
 
 	jest.unstable_mockModule('../src/fetcher.mjs', () => ({
-		default: {
-			getLocalIpAddressesContent: async (sysauthCookie) => { expect(sysauthCookie).toBeDefined(); return dnsServer },
-			getRouterStatusContent: async (sysauthCookie) => { expect(sysauthCookie).toBeDefined(); return status },
-		},
+		default: mockedExports,
 	}));
 }
